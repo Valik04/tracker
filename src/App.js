b04cb8bcb345bc977409b8df30784c9f133d1fb4
valik <martynenkovalik@gmail.com>
2022-07-07 15:14:43 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef ,useState} from "react";
 
 function App() {
+    const inputRef = useRef(null);
+    const [message, setMessage] = useState('');
+    const [list, setList] = React.useState([]);
+
+
+    function handleClick(e) {
+        e.preventDefault()
+        setMessage(inputRef.current.value);
+
+        const newList = list.concat(inputRef.current.value);
+        setList(newList);
+
+        inputRef.current.value = null
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='item'>
+          <h1>tracker</h1>
+        </div>
+
+        <form>
+          <input type="text" placeholder='Enter tracker name'  ref={inputRef} />
+
+
+                <ul>
+                    {list.map((item) => (
+                        <li>{item}
+                            <div className='tracker-buttons'>
+                                <button className='start'>x</button>
+                                <button className='stop'>x</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+
+
+
+          <button type='submit' onClick={handleClick}>
+            <span className="material-icons">
+                not_started
+            </span>
+          </button>
+        </form>
     </div>
   );
 }
